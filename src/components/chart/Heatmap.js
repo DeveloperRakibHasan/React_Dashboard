2d97703cfdfd27e89b044527a04b45379d9b2e66
rakib <mdrakibhasan240@gmail.com>
2022-07-24 16:55:47 +0600

    react heatmap add

--- src/components/chart/Heatmap.js
+++ src/components/chart/Heatmap.js
@@ -1,89 +1,74 @@
+// /* eslint-disable no-undef */
 import React, {useState} from 'react'
 import ReactApexChart from 'react-apexcharts'
 
-
 function Heatmap() {
-    const [heatmaps] = useState (
-        {
-            series: [{
-                name: 'Metric1',
-                data: (18, {
-                  min: 0,
-                  max: 90
-                })
-              },
-              {
-                name: 'Metric2',
-                data: (18, {
-                  min: 0,
-                  max: 90
-                })
-              },
-              {
-                name: 'Metric3',
-                data: (18, {
-                  min: 0,
-                  max: 90
-                })
-              },
-              {
-                name: 'Metric4',
-                data: (18, {
-                  min: 0,
-                  max: 90
-                })
-              },
-              {
-                name: 'Metric5',
-                data: (18, {
-                  min: 0,
-                  max: 90
-                })
-              },
-              {
-                name: 'Metric6',
-                data: (18, {
-                  min: 0,
-                  max: 90
-                })
-              },
-              {
-                name: 'Metric7',
-                data: (18, {
-                  min: 0,
-                  max: 90
-                })
-              },
-              {
-                name: 'Metric8',
-                data: (18, {
-                  min: 0,
-                  max: 90
-                })
-              },
-              {
-                name: 'Metric9',
-                data: (18, {
-                  min: 0,
-                  max: 90
-                })
-              }
-              ],
-              options: {
-                chart: {
-                  height: 350,
-                  type: 'heatmap',
+    const [heatmaps] = useState ({
+     
+        options: {
+          colors: [function({ value}) {
+            if (value < 300) {
+                return '#696CFF'
+            } else {
+                return '#A5A6FF'
+            }
+          }],
+          
+            chart: {
+                toolbar: {
+                    show: true
                 },
-                dataLabels: {
-                  enabled: false
-                },
-                colors: ["#008FFB"],
-                title: {
-                  text: 'HeatMap Chart (Single color)'
-                },
-              },
-          },    
-    )
+            },
+            dataLabels: {
+              enabled: false
+            },
+            xaxis: {
+              categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep'],
+                labels: {
+                    show: true,
+                }
+            },
+            tooltip: {
+                enabled: true,
+                //custom:
+            },
+         },
+         series: [{
+            name: ' ',
+            data: [30, 30, 30, 30, 30, 545, 30, 30]
+          },
+            {
+                name: ' ',
+                data: [30, 454, 30, 30, 30, 34, 30, 30]
+            },
+            {
+                name: ' ',
+                data: [30, 30, 454, 30, 30, 34, 30, 30]
+            },
+            {
+                name: ' ',
+                data: [30, 30, 30, 450, 450, 34, 30, 30]
+            },
+            {
+                name: ' ',
+                data: [30, 30, 30, 450, 450, 34, 30, 30]
+            },
+            {
+                name: ' ',
+                data: [30, 30, 30, 44, 34, 450, 30, 30]
+            },
+            {
+                name: ' ',
+                data: [30, 30, 30, 44, 34, 30, 450, 30,]
+            },
+            {
+                name: '',
+                data: [30, 30, 30, 44, 34, 30, 30, 30]
+            }]
+    
+    
+          })
+         
 
   return (
     <div className='w-full h-full bg-white box-shadow-1 rounded-xl xl:p-8 xm:p-6 xm:col-span-2 md:col-auto'>
@@ -97,7 +82,7 @@ function Heatmap() {
         </div>
         <p><span className='py-1 px-2 bg-green-300 rounded-md mr-2'>42.55%</span>Current Years</p>
         <div className='w-full'>
-        <ReactApexChart options={heatmaps.options} series={heatmaps.series} type="heatmap" height={350} />
+        <ReactApexChart options={heatmaps.options} series={heatmaps.series} type="heatmap" height={300} />
         </div>
     </div>
   )
