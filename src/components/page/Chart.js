99f1bd6a37ecef07b1c607a3c8bc66db0f4462c1
rakib <mdrakibhasan240@gmail.com>
2022-08-01 15:41:32 +0600

    User & Chart page loader set

--- src/components/page/Chart.js
+++ src/components/page/Chart.js
@@ -1,23 +1,45 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import CurrentRatting from '../chart/CurrentRatting'
 import ViewActivity from '../chart/ViewActivity'
 import ExpencesKeyword from '../home/ExpencesKeyword'
 import Heatmap from '../chart/Heatmap'
 import SellStatus from '../chart/SellStatus'
 import SearchCatagory from '../chart/SearchCatagory'
+import '../style/userLoader.css'
 
 function Chart() {
+    const [loading, setLoading] = useState(false)
+
+    useEffect(() => {
+        setLoading(true)
+        setTimeout(() => {
+            setLoading(false)
+        }, 1000)
+    }, [])
+
   return (
-    <div className='bg-[#F5F5F9] pt-[65px] pb-10 2xl:ml-[16.66%]'>
-        <div className='grid grid-flow-row lg:grid-cols-3 xm:grid-cols-2 2xl:gap-8 xm:gap-6 xl:ml-8 xm:ml-6 2xl:mt-8 xm:mt-6 xl:mr-8 xm:mr-6'>
-            <CurrentRatting />
-            <ViewActivity />
-            <ExpencesKeyword />
-            <Heatmap />
-            <SearchCatagory />
-            <SellStatus />
+   <>
+    {
+        loading ? 
+        <div className="flex w-full h-screen justify-center items-center pt-[65px]">
+            <div className="lds-ripple">
+                <div></div>
+                <div></div>
+            </div>
         </div>
-    </div>
+        :
+        <div className='bg-[#F5F5F9] pt-[65px] pb-10 2xl:ml-[16.66%]'>
+            <div className='grid grid-flow-row lg:grid-cols-3 xm:grid-cols-2 2xl:gap-8 xm:gap-6 xl:ml-8 xm:ml-6 2xl:mt-8 xm:mt-6 xl:mr-8 xm:mr-6'>
+                <CurrentRatting />
+                <ViewActivity />
+                <ExpencesKeyword />
+                <Heatmap />
+                <SearchCatagory />
+                <SellStatus />
+            </div>
+        </div>
+    }
+   </>
   )
 }
 
--- src/components/page/Users.js
+++ src/components/page/Users.js
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import userimg from '../../assets/img/2.png'
+import '../style/userLoader.css'
 
 function Users() {
 
     const [user, setUser] = useState([]);
+    const [loading, setLoading] = useState(false);
 
     useEffect(() => {
         const userData = async () => {
@@ -14,27 +16,47 @@ function Users() {
         userData();
     }, []);
 
+    useEffect(() => {
+        setLoading(true)
+        setTimeout(() => {
+            setLoading(false)
+
+        }, 2000)
+    }, [])
+
   return (
-    <div className='bg-[#F5F5F9] pt-[65px] pb-10 2xl:ml-[16.66%]'>
-        <div className='px-10 mt-10'>
-            <div className='w-full'>
-               {
-                user.map((users) => {
-                    return(
-                        <ul className='flex w-full items-center justify-between text-center bg-white px-10 py-4 mb-4 rounded-lg box-shadow'>
-                            <li className='flex gap-4 items-center font-bold'>{users.id}<img className='w-8 h-8 rounded-full' src={userimg} alt='' /></li>
-                            <li className=' uppercase'>{users.name}</li>
-                            <li>{users.email}</li>
-                            <li>{users.phone}</li>
-                            <li>{users.website}</li>
-                        </ul>
-                    )
-                })
-               }
-               
-            </div>
-        </div>
-    </div>
+     <>
+         {
+             loading ?
+                 <div className="flex w-full h-screen justify-center items-center pt-[65px]">
+                     <div className="lds-ripple">
+                         <div></div>
+                         <div></div>
+                     </div>
+                 </div>
+                 :
+                 <div className='bg-[#F5F5F9] pt-[65px] pb-10 2xl:ml-[16.66%]'>
+                     <div className='px-10 mt-10'>
+                         <div className='w-full'>
+                             {
+                                 user.map((users) => {
+                                     return(
+                                         <ul className='flex w-full items-center justify-between text-center bg-white px-10 py-4 mb-4 rounded-lg box-shadow'>
+                                             <li className='flex gap-4 items-center font-bold'>{users.id}<img className='w-8 h-8 rounded-full' src={userimg} alt='' /></li>
+                                             <li className=' uppercase'>{users.name}</li>
+                                             <li>{users.email}</li>
+                                             <li>{users.phone}</li>
+                                             <li>{users.website}</li>
+                                         </ul>
+                                     )
+                                 })
+                             }
+                         </div>
+                     </div>
+                 </div>
+         }
+     </>
+
   )
 }
 
